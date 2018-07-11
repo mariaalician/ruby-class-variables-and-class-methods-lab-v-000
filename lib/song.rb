605beb65248a3b8b29bc55e7b402c32fc223fe09
Maria Nunez <mariaalician@gmail.com>
2018-07-11 00:50:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,10 @@ class Song
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
-      else genre_count[genre] = 1
+      else 
+        genre_count[genre] = 1
+  end
+  
   end
 
   def self.artist_count
